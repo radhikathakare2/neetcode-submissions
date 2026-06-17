419ee7adcfa50e0811bed723016326b48b9ad847
radhikathakare2 <130585434+radhikathakare2@users.noreply.github.com>
2026-06-17 23:00:18 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+
+        string ans = "";
+
+        for(string str : strs) {
+
+            ans += to_string(str.length());
+
+            ans += '#';
+
+            ans += str;
+        }
+
+        return ans;
+    }
+
+    vector<string> decode(string s) {
+
+        vector<string> ans;
+
+        int i = 0;
+
+        while(i < s.size()) {
+
+            int j = i;
+
+            while(s[j] != '#') {
+
+                j++;
+            }
+
+            int len = stoi(s.substr(i, j-i));
+
+            string word = s.substr(j+1, len);
+
+            ans.push_back(word);
+
+            i = j + 1 + len;
+        }
+
+        return ans;
+    }
+};
+
+
