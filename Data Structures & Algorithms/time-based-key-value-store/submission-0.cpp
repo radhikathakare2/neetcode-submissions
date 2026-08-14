86b692432818b15d836754ae3aeb4f0ef4292f75
radhikathakare2 <130585434+radhikathakare2@users.noreply.github.com>
2026-08-14 21:33:17 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,45 @@
+class TimeMap {
+public:
+
+    unordered_map<string, vector<pair<int, string>>> mp;
+
+    TimeMap() {
+    }
+
+    void set(string key, string value, int timestamp) {
+
+        mp[key].push_back({timestamp, value});
+    }
+
+    string get(string key, int timestamp) {
+
+        if (mp.find(key) == mp.end())
+            return "";
+
+        vector<pair<int, string>>& v = mp[key];
+
+        int low = 0;
+        int high = v.size() - 1;
+
+        string ans = "";
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (v[mid].first <= timestamp) {
+
+                ans = v[mid].second;
+
+                // Try to find a larger valid timestamp
+                low = mid + 1;
+            }
+            else {
+
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
